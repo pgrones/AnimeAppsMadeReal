65a26b78960e79749c79bb8ac9fe9909315e7671
pgr3931 <45991112+pgr3931@users.noreply.github.com>
2022-01-08 20:43:10 +0100

    Landingpage

--- src/components/common/AniList.tsx
+++ src/components/common/AniList.tsx
@@ -18,8 +18,8 @@ const AniList = () => {
     }, [])
 
     return (
-        <a href="https://anilist.co/user/Alzariel/" target="_blank" rel="noopener noreferrer" title="Visit my AniList profile" className="mr-3 cursor-pointer hover:scale-[1.2] active:scale-100 transition-transform">
-            <svg width="2.1rem" height="2.1rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+        <a href="https://anilist.co/user/Alzariel/" target="_blank" rel="noopener noreferrer" title="Visit my profile on AniList" className="mr-3 cursor-pointer hover:scale-[1.2] active:scale-100 transition-transform">
+            <svg width="1.8rem" height="1.8rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
                 <defs>
                     <linearGradient id="gradient">
                         <stop offset="0%" stopColor={dark ? "#ec4899" : "#8b5cf6"} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   mode: 'jit',
   darkMode: 'class',
   content: [
     './public/**/*.html',
     './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
-  ]
+  ],
+  theme: {
+    screens: {
+      'xs': '475px',      
+      ...defaultTheme.screens
+    }
+  }
 };
